9e2c1cd97b9b2f7bcb4437a2b8a9fd5b906bb2c8
Adam Taylor <adamjctaylor@gmail.com>
2011-04-11 23:36:25 +0100

    Start talking to our sqlite database

--- laurabelle.js
+++ laurabelle.js
@@ -1,19 +1,52 @@
-var jerk = require( 'jerk' )
+var jerk = require( 'jerk' ),
+sys      = require( 'sys' ),
+sqlite   = require( './vendor/sqlite' );
 
 var options = {
     server: 'irc.freenode.net',
     nick: 'Laurabelle', 
-    channels: [ '#kkkk',  ]
+    channels: [ '#kkkk' ]
 }
 
 jerk( function( j ) {
+    var db = new sqlite.Database();
 
-  j.watch_for( 'soup', function( message ) {
-    message.say( message.user + ': soup is good food!' )
-  })
+    j.watch_for( 'soup', function( message ) {
+        message.say( message.user + ': soup is good food!' )
+    })
 
-  j.watch_for( /^(.+) is (.*)/, function( message ) {
-    message.say( 'Okay, ' + message.user + ': ' + message.match_data[1] + ' is ' + message.match_data[2] )
-  })
+    j.watch_for( /^(.+) is (.*)/, function( message ) {
+        message.say( 'Okay, ' + message.user + ': ' + message.match_data[1] + ' is ' + message.match_data[2] )
+        db.open("info.db", function (error) {
+            if (error) {
+                throw error;
+            }
+            db.execute(
+                "INSERT INTO factoids (thing,fact) VALUES (?,?)",
+                [message.match_data[1], message.match_data[2]],
+                function (error, rows) {
+                      if (error) throw error;
+                }
+            );
+        });
+    })
+
+    j.watch_for( /^(.+)?/, function( message ) {
+        db.open("info.db", function (error) {
+            if (error) {
+                throw error;
+            }
+            db.execute(
+                "SELECT fact FROM factoids WHERE thing = ?",
+                [message.match_data[1]],
+                function (error, rows) {
+                    if (error) {
+                        throw error;
+                    }
+                    message.say( message.user + ':' +  message.match_data[1] + ' is ' + rows[0].fact )
+                }
+            );
+        });
+    })
 
 }).connect( options )
